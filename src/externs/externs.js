649396875c8f45ec6ef8e99ad5d12d5f537f9afa
Anders Törnqvist <anders.tornqvist@gmail.com>
2017-10-26 14:56:40 +0200

    Change to compilations_level simple

---
+++ src/externs/externs.js
@@ -0,0 +1,4 @@
+/**
+ * @namespace
+ */
+var firebase = {};
--- support/webpack.prod.config.js
+++ support/webpack.prod.config.js
@@ -12,9 +12,9 @@ module.exports = {
             compiler: {
                 language_in: 'ECMASCRIPT5_STRICT', // 'ECMASCRIPT6',
                 language_out: 'ECMASCRIPT5_STRICT', // IE11, EDGE15, 98%
-                compilation_level: 'ADVANCED', // WHITESPACE_ONLY, SIMPLE, ADVANCED (default: SIMPLE)
+                compilation_level: 'SIMPLE', // WHITESPACE_ONLY, SIMPLE, ADVANCED (default: SIMPLE)
                 warning_level: 'verbose',
-                // externs: './src/externs/closure.js',
+                externs: './src/externs/externs.js',
                 isolation_mode: 'NONE' // NONE | IIFE  (default: NONE)
             },
             concurrency: 3
