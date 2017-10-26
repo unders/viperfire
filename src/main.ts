a6488ca2e8e4efa4f2a6ec0bfe664909775c7085
Anders Törnqvist <anders.tornqvist@gmail.com>
2017-10-26 14:32:49 +0200

    add firebase to project.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
-import {bind, wire} from './dom/dom'
+import { bind, wire } from './dom/dom'
+import * as firebase from "firebase";
 
-const main = () => {
+const main = async () => {
+    // Initialize Firebase
+
+    const resp = await fetch('/__/firebase/init.json');
+    const json = await resp.json();
+    const fireapp = firebase.initializeApp(json);
+
+    const dbRef = fireapp.database().ref().child('text');
+    dbRef.on('value', (snap) => {
+        if (snap) {
+            console.log(snap.val());
+        }
+    });
+
+    console.log("it works");
+
+
     const app = document.getElementById("app");
     if (app) {
         bind(app)`<div>It works</div>${wire()`<p>Hello world</p>`}`
--- support/webpack.dev.config.js
+++ support/webpack.dev.config.js
@@ -20,7 +20,7 @@ module.exports = {
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
-    }
+    },
 
     // Other options...
     //
@@ -29,9 +29,9 @@ module.exports = {
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
-    //externals: {
-    //    "firebase": "firebase"
+    externals: {
+        "firebase": "firebase"
     //    "react": "React",
     //    "react-dom": "ReactDOM"
-    // }
+    }
 };
