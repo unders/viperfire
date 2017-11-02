6707621f443d45c1e792119ead48536f15b36227
Anders Törnqvist <anders.tornqvist@gmail.com>
2017-11-02 19:53:38 +0100

    fix Cache-Control header

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 
     res.set("Content-Type", "text/html; charset=utf-8");
     if (config.isOnline) {
-        res.set("Cache-Control", "public, max-age=1 s-max-age=1");
+        res.set("Cache-Control", "public, max-age=1, s-max-age=1");
     }
 
     const { body, err } = page.articleList("/", "Hello World");
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
     res.set("Content-Type", "text/html; charset=utf-8");
     if (config.isOnline) {
-        res.set("Cache-Control", "public, max-age=10 s-max-age=100");
+        res.set("Cache-Control", "public, max-age=10, s-max-age=100");
     }
     const { body, err } = page.about("/about");
     if (err) {
@@ -47,7 +47,7 @@ app.get("/about", (req, res) => {
 
 app.get("/article/:id", (req, res) => {
     if (config.isOnline) {
-        res.set("Cache-Control", "public, max-age=1 s-max-age=2");
+        res.set("Cache-Control", "public, max-age=1, s-max-age=2");
     }
     res.status(200).send(`article/${req.params.id}\n`);
 });
