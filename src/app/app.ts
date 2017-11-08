03bcb1e6720b65d9c5c876b41b7e453d7b393bb4
Anders Törnqvist <anders.tornqvist@gmail.com>
2017-11-08 10:53:11 +0100

    App page transitions to app module

--- src/app/app.ts
+++ src/app/app.ts
@@ -27,30 +27,44 @@ export class App {
     //
     // Page transitions
     //
-    rootVisit() {
-        console.log("rootVisit");
-    }
-    rootBack() {
-        console.log("rootBack");
+
+    rootVisit() { this.root("rootVisit"); }
+    rootBack() { this.root("rootBack"); }
+    root(msg: string) {
+        this.logger.info(msg);
+        const ctx  = { path: "/", user: this.state.user, articleList: { message: "Hello World"} };
+        const state = new ArticleListState(ctx);
+        this.state = state;
+        this.view.renderArticleList(this.html, state);
     }
 
-    aboutVisit() {
-        console.log("aboutVisit");
-    }
-    aboutBack() {
-        console.log("aboutBack");
-    }
-
-    profileVisit(uid: string) {
-        console.log("profileVisit");
+    aboutVisit() { this.about("aboutVisit"); }
+    aboutBack()  { this.about("aboutBack"); }
+    about(msg: string) {
+        this.logger.info(msg);
+        this.state = new AboutState('/about', this.state.user);
+        this.view.renderAbout(this.html, this.state);
     }
 
-    profileBack(uid: string) {
-        console.log("profileBack");
+    profileVisit(uid: string) { this.profile(uid, "profileVisit"); }
+    profileBack(uid: string)  { this.profile(uid, "profileBack"); }
+    profile(uid: string, msg: string) {
+        this.logger.info(msg);
+        const currentUser = this.state.user;
+        let name = this.state.user.name;
+        if (currentUser.uid !== uid) {
+            // if user not found show 404 page.
+            // domain/profile/profile.get(uid): User
+            name  = "we should get this from the server or a local cache";
+        }
+
+        const state = new ProfileState({path: "/profile/:uid", user: currentUser, ctx: { name: name }});
+        this.state = state;
+        this.view.renderProfile(this.html, state);
     }
 
     notFound(path: string) {
-        console.log("notFound");
+        this.logger.info(path);
     }
 
     //
@@ -65,7 +79,7 @@ export class App {
     // Render current page
     //
     render() {
-        this.logger.info("render page");
+        this.logger.info(`render page=${this.state.path}`);
 
         if (this.state.path === "/") {
             const state = this.state as ArticleListState;
