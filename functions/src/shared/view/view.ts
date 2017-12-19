688b9a364050a64090b4215653639eb4c317878d
Anders Törnqvist <anders.tornqvist@gmail.com>
2017-12-19 11:49:13 +0100

    add snackbar styling.

--- functions/src/shared/view/view.ts
+++ functions/src/shared/view/view.ts
@@ -65,6 +65,20 @@ export class View {
         return html`
             ${[this.pageLoader.render(p)]}
             ${[this.popup.render(p)]}
+            <div class="snackbar hide">
+                <div class="snackbar-container">
+                    <span class="snackbar-message">You are signed in as anders.tornqvist@gmail.com</span>
+                    <span class="snackbar-action">
+                        <a href="#" class="hide">Undo</a>
+                     </span>
+                    <span class="snackbar-close">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" height="24" viewBox="0 0 24 24" width="24">
+                            <path class="clear" d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+                            <path d="M0 0h24v24H0z" fill="none"/>
+                        </svg>
+                    </span>
+                </div>
+            </div>
             </div>
             <div class="container">
                 <header>${[this.header.render(p)]}</header>
